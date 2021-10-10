2b87f06629d2f26dbb763169ceb525adeaab3530
PiotrWojtalewicz <piowojt19@gmail.com>
2021-10-10 19:17:59 +0200

    ab

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Cześć, jestem Piotr");
+        System.out.print("Jak się masz?");
 
     }
 }
