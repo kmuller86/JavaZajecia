e52fbd53dd5a9b3fd647e458e502ef4bffaf7eb6
PiotrWojtalewicz <piowojt19@gmail.com>
2021-10-27 16:28:23 +0200

    W końcu się udało !!!

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.4 */
+                Scanner scan = new Scanner(System.in);
+                String name = scan.nextLine();
+                String secondName= scan.nextLine();
+                int age = scan.nextInt();
+                int indeks = scan.nextInt();
+                System.out.println("Cześć! Nazywam się " + name + secondName);
+                System.out.println("Mam " + age + " lat");
+                System.out.println("A mój numer indeksu to: " + indeks);
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+            }
+        }
 
-    }
-}
+        /* ćw.3 */
+
+        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
+        *   a) Imię, nazwisko, wiek, nr indeksu,
+        *   dane mają być wprowadzane z klawiatury w konsoli
+        *   b) wyświetlić dane za pomocą println i printf
+        */
+
+
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+        *   wynik wyświetlić w konsoli programu
+        */
+
+
